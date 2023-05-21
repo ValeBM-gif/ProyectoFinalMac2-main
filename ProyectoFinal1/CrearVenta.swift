3f8c3196f5e8803ee20b827459d3495801dbe9bc
Uriel Resendiz <UrielRe46@users.noreply.github.com>
2023-05-20 19:04:45 -0600

    validación agregar ventas

--- ProyectoFinal1/CrearVenta.swift
+++ ProyectoFinal1/CrearVenta.swift
@@ -13,34 +13,107 @@ class CrearVenta: NSViewController {
     @objc dynamic var ventasLog:[VentaModelo] = []
     var idUsuarioActual:Int!
     @IBOutlet weak var btnAgregarVenta: NSButton!
+    @IBOutlet weak var txtIdProducto: NSTextField!
+    @IBOutlet weak var txtCantidad: NSTextField!
+    @IBOutlet weak var lblIncorrecto: NSTextField!
+    @IBOutlet weak var lblSubtotalVenta: NSTextField!
+    @IBOutlet weak var lblTotalVenta: NSTextField!
     
-    //TO DO: Funcionalidad de agregar venta a partir de un botón
-    //TO DO: NO SE PUEDE REPETIR ID DE VENTA
-    //TO DO: CONECTAR VENTAS A PEDIDOS PARA QUE EL CLIENTE TENGA ACCESO
-   
+    //TODO: Funcionalidad de agregar venta a partir de un botón
+    //TODO: NO SE PUEDE REPETIR ID DE VENTA
+    //TODO: CONECTAR VENTAS A PEDIDOS PARA QUE EL CLIENTE TENGA ACCESO
+    
+    var idProducto: Int=0
+    var cantidadProducto: Int=0
+    var totalProducto: Double=0
+    var totalVenta:Double=0
+    var subtotalVenta:Double=0
+    var precio1: Double=100
+    var precio2: Double=200
+    
     override func viewDidLoad() {
-       
-        ventasLog.append(VentaModelo(idVenta: 1, idVendedor: 1, idCliente: 1, idProducto: 2, cantidad: 1, precioProducto: 100, totalProducto: 0, subtotalVenta: 0, ivaVenta: 10, totalVenta: 0))
-        
         super.viewDidLoad()
+        lblIncorrecto.isHidden = true
         
     }
     
     @IBAction func agregarVenta(_ sender: NSButton) {
-        ventasLog.append(VentaModelo(idVenta: 10, idVendedor: 1, idCliente: 1, idProducto: 2, cantidad: 1, precioProducto: 100, totalProducto: 0, subtotalVenta: 0, ivaVenta: 10, totalVenta: 0))
+        if validarCamposVacios(){
+            if txtCantidad.stringValue != ""{
+                if txtIdProducto.stringValue != ""{
+                    if soloHayNumerosEnCantidad(){
+                        cantidadProducto = txtCantidad.integerValue
+                        lblIncorrecto.isHidden = true
+                        if soloHayNumerosEnIdProducto(){
+                            idProducto = txtIdProducto.integerValue
+                            lblIncorrecto.isHidden = true
+                            if checarIdRepetido(id:txtIdProducto.integerValue){
+                                ventasLog.append(VentaModelo(idVenta: 1, idVendedor: 1, idCliente: txtIdProducto.integerValue, idProducto: 1, cantidad: txtCantidad.integerValue, precioProducto: precio1, totalProducto: calcularTotalProducto(id: txtIdProducto.integerValue), subtotalVenta: 100, ivaVenta: 10, totalVenta: 100))
+                            }else{
+                                lblIncorrecto.stringValue = "*Inserta un ID diferente*"
+                                lblIncorrecto.isHidden = false
+                            }
+                        }
+                    }
+                }else{
+                    lblIncorrecto.stringValue = "*Inserta un ID*"
+                    lblIncorrecto.isHidden = false
+                }
+            }else{
+                lblIncorrecto.stringValue = "*Inserta una cantidad*"
+                lblIncorrecto.isHidden = false
+            }
+        }else{
+            lblIncorrecto.stringValue = "*Inserta un ID y una cantidad*"
+            lblIncorrecto.isHidden = false
+        }
     }
     
-    func calcularTotalProducto(){
-        //cantidad*precioProducto
+    func calcularTotalProducto(id:Int)->Double{
+        for venta in ventasLog{
+            if(venta.idProducto == id){
+                totalProducto = venta.precioProducto * txtCantidad.doubleValue
+            }
+        }
+        return totalProducto
     }
     
-    func calcularTotalVenta(){
+    func calcularTotalVenta(id:Int){
       //Se compara el id de venta actual para encontrar todos los productos de la venta, se suman todos los totalesProductos y se obtiene totalVenta
     }
     
-    func calcularSubtotalVenta(){
-        //  totalVenta/1.16
+    func calcularSubtotalVenta(id:Int)->Double{
+        for venta in ventasLog{
+            if(venta.idVenta == id){
+                
+            }
+        }
+        return totalVenta
     }
     
+    func checarIdRepetido(id:Int) -> Bool{
+        for i in ventasLog{
+            if (i.idProducto == id){
+                return false
+            }
+        }
+        return true
+    }
+        
+    func validarCamposVacios() -> Bool{
+        if(txtCantidad.stringValue == "" && txtIdProducto.stringValue == ""){
+            return false
+        }
+        return true
+    }
     
+    func soloHayNumerosEnIdProducto() -> Bool{
+        let numericCharacters = CharacterSet.decimalDigits.inverted
+        return txtIdProducto.stringValue.rangeOfCharacter(from: numericCharacters) == nil
+    }
+    
+    func soloHayNumerosEnCantidad() -> Bool{
+        let numericCharacters = CharacterSet.decimalDigits.inverted
+        return txtCantidad.stringValue.rangeOfCharacter(from: numericCharacters) == nil
+    }
 }
